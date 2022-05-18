5cc6230dbe3f66cd463ded751148f339b71d0e93
anados9706 <anados9706@granitesd.org>
2022-05-18 08:46:47 -0600

    with the sound

--- pyGame.py
+++ pyGame.py
@@ -1,5 +1,7 @@
-import pygame,sys
+import pygame,sys,time
 screen_width,screen_height = 800,800
+
+from pygame.locals import *
 
 pygame.init()
 
@@ -9,6 +11,7 @@ bg = pygame.transform.scale(bg,(screen_width,screen_height))
 path_1 = [((30,450),(200,300)),((30,450),(450,230)),((330,150),(150,500)),((330,150),(350,30)),((600,30),(80,150))]
 path_2 = [((35,75),(720,120)),((35,75),(120,350)),((35,400),(340,100)),((290,285),(85,215)),((290,285),(465,70)),((700,285),(60,330)),((35,530),(700,85)),((35,530),(200,250)),((35,700),(730,80))]
 path_3 = [((40,650),(700,70)),((40,550),(70,100)),((40,550),(680,60)),((660,450),(60,100)),((50,450),(660,50)),((50,350),(50,100)),((50,350),(650,50)),((650,300),(50,50)),((60,280),(640,35)),((60,250),(35,50)),((60,230),(600,35)),((640,190),(20,40)),((80,190),(570,20)),((80,170),(16,30)),((80,160),(570,16)),((638,140),(12,20)),((350,140),(300,12)),((350,120),(12,20)),((335,70),(50,50))]
+sound = pygame.mixer.Sound("scream.wav")
 run=True
 
 pygame.mouse.set_pos((100,600))
@@ -60,13 +63,10 @@ def Level_2():
         screen.blit(bg,(0,0))
         if ((pygame.mouse.get_pos()[0]-615)**2+(pygame.mouse.get_pos()[1]-130)**2)<=100:
             Level_3()
-            #msg = font.render("you won!!",1,(255,255,0)) 
-            #screen.blit(msg,(300,100))
-
 
         for x in path_2:
-            pygame.draw.rect(screen,(255,255,255),x)
-        pygame.draw.circle(screen,(225,0,0),(615,130),10)
+            pygame.draw.rect(screen,(0,255,255),x)
+        pygame.draw.rect(screen,(225,48,48),x)
         pygame.display.update()     
 
 
@@ -88,14 +88,12 @@ def Level_3():
         screen.fill((0, 0, 0))
         screen.blit(bg,(0,0))
         if ((pygame.mouse.get_pos()[0]-615)**2+(pygame.mouse.get_pos()[1]-130)**2)<=100:
-            
+            sound.play ()
+            time.sleep(1)
 
-         for x in path_3:
-            pygame.draw.rect(screen,(255,255,255),x)
-        pygame.draw.circle(screen,(225,0,0),(615,130),10)
+        for x in path_3:
+            pygame.draw.rect(screen,(0,255,255),x)
+        pygame.draw.rect(screen,(225,48,48),x)
         pygame.display.update()     
 
-
-Level_1()     
-Level_2()     
-Level_3()     
+Level_1()          
